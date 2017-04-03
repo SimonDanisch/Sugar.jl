70de839d2eafc838917b15dc64a91a4fe603d1dc
SimonDanisch <sdanisch@gmail.com>
2017-04-04 01:03:52 +0200

    fix error from better map typeinf on 0.6

--- src/sugarcoating.jl
+++ src/sugarcoating.jl
@@ -21,7 +21,7 @@ Removes all goto/label constructs and puts them back into Julia Expr
 function remove_goto(ast)
     ast = matchreplace(ast, while_pattern) do loop_label, unless, whilebody, continue_label, goto, break_label
         condition = unless[1].args[1]
-        whilebody = replace_continue_break(collect(whilebody), continue_label[1], break_label[1])
+        whilebody = Any[replace_continue_break(collect(whilebody), continue_label[1], break_label[1])...]
         push!(whilebody, continue_label[1])
         whilebody = remove_goto(whilebody)
         if last(whilebody) == continue_label[1] # TODO, I guess this will never be removed anyways, so we could just always pop it
