661dd46433aa0f22a90e82886003e31ce7e7047d
SimonDanisch <sdanisch@gmail.com>
2017-09-11 08:54:28 -0400

    get pointer tracking working

--- src/methods.jl
+++ src/methods.jl
@@ -35,6 +35,15 @@ LazyMethod{T}(lm::LazyMethod{T}, t) = LazyMethod{T}(t, lm, lm.cache)
 LazyMethod(signature::DataType) = LazyMethod{:JL}(signature)
 LazyMethod(f, types) = LazyMethod{:JL}((f, Base.to_tuple_type(types)))
 
+"""
+Like @code_typed, but will create a lazymethod!
+"""
+macro lazymethod(ex0)
+    :($(Base.gen_call_with_extracted_types(:LazyMethod, ex0)))
+end
+
+
+
 
 function isfunction(x::LazyMethod)
     isa(x.signature, Tuple) &&
@@ -63,6 +72,7 @@ function Base.push!{T, T2}(decl::LazyMethod{T}, signature::T2)
     push!(decl.dependencies, LazyMethod(decl, signature))
 end
 
+# Make Dicts/Set work correctly
 import Base: ==
 Base.hash(x::LazyMethod, h::UInt64) = hash(x.signature, h)
 ==(x::LazyMethod, y::LazyMethod) = x.signature == y.signature
@@ -104,8 +114,8 @@ function newslot!(m, T, name = gensym())
     slot = (T, name)
     idx = findfirst(m.slots, slot)
     if idx > 0
-        # means we already have a slot with this name, which we can use.
-        # This needs to be treated with care, since it might also be a clash
+        # means we already have a slot with this name + type, which we can use.
+        # This needs to be treated with care, since it might also be a clash.
         return TypedSlot(idx, T)
     else
         push!(m.slots, (T, name))
@@ -120,8 +130,7 @@ function getslots!(m::LazyMethod)
         ci = getcodeinfo!(m)
         slots = Tuple{DataType, Symbol}[]
         for (i, (T, name)) in enumerate(zip(ci.slottypes, ci.slotnames))
-            # tmp must be made unique
-            # TODO check if this is just because I mess up scope!
+            # tmp + unused must be made unique
             if name == Symbol("#temp#")
                 name = Symbol("xtempx_", i)
             elseif name == Symbol("#unused#")
@@ -140,8 +149,8 @@ slotnames(m::LazyMethod) = map(last, getslots!(m))
 slottype(m::LazyMethod, s::TypedSlot) = s.typ
 slottype(m::LazyMethod, s::Slot) = first(getslots!(m)[s.id])
 slottype(m::LazyMethod, s::SSAValue) = ssatypes(m)[s.id + 1]
-
 slotname(tp::LazyMethod, s::SSAValue) = ssavalue_name(s)
+
 function slotname(m::LazyMethod, s::Slot)
     slots = getslots!(m)
     id = s.id
@@ -155,6 +164,7 @@ end
 
 function returntype(x::LazyMethod)
     if isclosure(x.signature[1])
+        # TODO isn't there an easier way?!
         ftype = x.signature[1]
         world = typemax(UInt)
         tt = Tuple{ftype, to_tuple(x.signature[2])...}
@@ -172,6 +182,7 @@ function method_nargs(f::LazyMethod)
     method = getmethod!(f)
     method.nargs
 end
+
 function type_ast(T)
     fields = Expr(:block)
     mutable = T <: Tuple ? false : T.mutable
@@ -185,10 +196,9 @@ end
 
 
 function has_varargs(x::LazyMethod)
-    if isintrinsic(x)
-        # we can't introspect intrinsics, for all I know
-        return false, 0
-    end
+    # we can't introspect intrinsics, for all I know
+    isintrinsic(x) && return false, 0
+
     n = method_nargs(x)
     calltypes, real_signature = to_tuple(x.signature[2]), to_tuple(slottypes(x)[2:(n)])
     if calltypes == real_signature
@@ -207,21 +217,24 @@ function getfuncargs(x::LazyMethod)
     functype = x.signature[1]
     calltypes, slots = to_tuple(x.signature[2]), getslots!(x)
     n = method_nargs(x)
-    start = ifelse(isclosure(functype), 1, 2)
-    args = map(start:n) do i
+    args = map(2:n) do i
         argtype, name = slots[i]
         # Slot types might be less specific, e.g. when the variable is unused it might end up as Any.
         # but generally the slot type is the correct one, especially in the context of varargs.
         calltype = if !isleaftype(argtype) && length(calltypes) <= i
             argtype = calltypes[i - 1]
         end
-
         expr = :($(name)::$(argtype))
         expr.typ = argtype
         expr
     end
     args
 end
+
+"""
+OpenCL needs to decompose structs containing device pointers, since they can't be inlined into structs.
+"""
+needs_decompose(x, T) = false
 
 function getast!(x::LazyMethod)
     if !isdefined(x, :ast)
@@ -241,6 +254,10 @@ function getast!(x::LazyMethod)
                         tmp = :($name::$T)
                         tmp.typ = T
                         unshift!(expr.args, tmp)
+                    else # argument
+                        if needs_decompose(x, T)
+                            cache[slot] = gensym(name)
+                        end
                     end
                 end
                 expr.typ = returntype(x)
@@ -423,13 +440,12 @@ function rewrite_ast(m, expr)
                 return true, param
             else
                 T = expr.typ
+                # Tuples containing types (.., Float32, ...) don't get specialized, but we can fix this
+                # because inference basically has this information.
                 if T <: Tuple && T != Union{}
-                    types = try
-                        to_tuple(T)
-                    catch e
-                        print_stack_trace(STDOUT, m)
-                        rethrow(e)
-                    end
+                    types = to_tuple(T)
+                    # if is call to tuple + has datatype in there, we can use the
+                    # type of it's arguments
                     if any(x-> x == DataType, types) && expr.head == :call && expr.args[1] == GlobalRef(Core, :tuple)
                         expr.typ = Tuple{map(x-> Sugar.expr_type(m, x), expr.args[2:end])...}
                     end
@@ -483,6 +499,7 @@ function rewrite_ast(m, expr)
                     types = (map(x-> expr_type(m, x), args[2:end])...)
                     FT = Sugar.expr_type(m, func)
                     return_type = expr_type(m, expr)
+
                     f = if isclosure(FT)
                         insert!(args, 2, func) # add self reference to call
                         FT
@@ -531,6 +548,9 @@ function rewrite_ast(m, expr)
                         # recursively rewrite arguments
                         result.args = map(x-> rewrite_ast(m, x), result.args)
                     end
+                    if needs_decompose(m, return_type)
+                        m.cache[expr] = rhs
+                    end
                     return true, result
                 end
             end
@@ -540,12 +560,12 @@ function rewrite_ast(m, expr)
             if isa(expr, LazyMethod)
                 assert_validity(expr)
                 push!(m, expr)
-            elseif isa(expr, TypedSlot) || isa(expr, SSAValue)
+            elseif isa(expr, Union{TypedSlot, SSAValue})
                 push!(m, expr_type(m, expr))
             end
         catch e
             println(STDERR, "___________________________________________________________________")
-            println(STDERR, "Error in Expr rewrite! This error might be ignored:")
+            println(STDERR, "Error in Expr rewrite! $e")
              # TODO filter errors, there are definitely errors that we can pick out that needs to be rethrown
             println(STDERR)
             println(STDERR, "Expression resulting in the error: ")
@@ -670,13 +690,8 @@ function getfuncheader!(x::LazyMethod)
 end
 
 function getfuncsource(x::LazyMethod)
-    try
-        ast, str = get_source(getmethod!(x))
-        str
-    catch e
-        sprint() do io
-            Base.show_unquoted(io, getast!(x), 0, 0)
-        end
+    sprint() do io
+        Base.show_unquoted(io, getast!(x), 0, 0)
     end
 end
 
@@ -700,26 +715,16 @@ function print_stack_trace(io, x::LazyMethod)
 end
 
 function getbodysource!(x::LazyMethod)
-    try
-        if istype(x)
-            gettypesource(x)
-        else
-            getfuncsource(x)
-        end
-    catch e
-        print_stack_trace(STDERR, x)
-        rethrow(e)
+    if istype(x)
+        gettypesource(x)
+    else
+        getfuncsource(x)
     end
 end
 
 function getsource!(x::LazyMethod)
     if !isdefined(x, :source)
-        try
-            str = getbodysource!(x)
-        catch e
-            print_stack_trace(STDERR, x)
-            rethrow(e)
-        end
+        str = getbodysource!(x)
         if isfunction(x)
             str = getfuncheader!(x) * "\n" * str
         end
@@ -763,7 +768,7 @@ A concrete function!
 """
 function resolve_func(m, f::LazyMethod)
     if isfunction(f)
-        Sugar.getfunction(f)
+        getfunction(f)
     else
         f.signature
     end
@@ -772,8 +777,8 @@ resolve_func(m, f::AllFuncs) = f
 resolve_func{T}(m, X::Type{Type{T}}) = T
 resolve_func{T}(m, X::Type{T}) = X
 resolve_func(m, f::Union{GlobalRef, Symbol}) = eval(f)
-function resolve_func(m, slot::Union{Slot, SSAValue})
-    f = expr_type(m, slot)
+function resolve_func(m, val::Union{Slot, SSAValue, Expr})
+    f = expr_type(m, val)
     if isclosure(f)
         return resolve_func(m, f)
     else
@@ -784,21 +789,8 @@ function resolve_func(m, slot::Union{Slot, SSAValue})
         end
     end
 end
-function resolve_func(m, f::Expr)
-    T = expr_type(m, f)
-    if T <: AllFuncs
-        return instance(T)
-    else
-        T
-    end
-end
 
-"""
-Like @code_typed, but will create a lazymethod!
-"""
-macro lazymethod(ex0)
-    :($(Base.gen_call_with_extracted_types(:LazyMethod, ex0)))
-end
+
 
 function replace_slots(m::LazyMethod, ast)
     first(Sugar.replace_expr(ast) do expr
---
+++ src/pointer_tracking.jl
@@ -0,0 +1,193 @@
+using Sugar
+using Sugar: replace_expr, expr_type, resolve_func, to_tuple, LazyMethod
+"""
+Interface to indicate that a type needs tracking.
+"""
+is_tracked_type(m, T) = false
+
+"""
+Figures out if a type `T` contains any type for which `is_tracked_type` returns true.
+Returns if it contains a tracked type and the fields with the types in the form of:
+`[(field1, nested_field, tracked_field)]`
+"""
+function contains_tracked_type(
+        m,
+        ::Type{T},
+        parent_field = (),
+        trackedfields = []
+    ) where T
+    has_tracked_field = false
+    for fname in fieldnames(T)
+        FT = fieldtype(T, fname)
+        if is_tracked_type(m, FT)
+            has_tracked_field = true
+            push!(trackedfields, (parent_field..., fname))
+        else
+            _has_tracked_field, trackedfields = contains_tracked_type(m, FT, (fname,), trackedfields)
+            has_tracked_field |= _has_tracked_field
+        end
+    end
+    has_tracked_field, trackedfields
+end
+
+needs_tracking(m, ::Type{T}) where T = contains_tracked_type(m, T)[1] || is_tracked_type(m, T)
+
+track_types!(m, any::ANY, pointer_map) = pointer_map
+
+get_fields_type(T, fields::NTuple{1}) = fieldtype(T, first(fields))
+get_fields_type(T, fields::NTuple{N}) where N = get_fields_type(fieldtype(T, first(fields)), Base.tail(fields))
+
+get_fields(x, fields::NTuple{1}) = getfield(x, first(fields))
+get_fields(x, fields::NTuple{N}) where N = get_fields(getfield(x, first(fields)), Base.tail(fields))
+
+
+function track_types!(m, args::Vector, pointer_map)
+    for elem in args
+        track_types!(m, elem, pointer_map)
+    end
+    pointer_map
+end
+
+push_app!(vec, elem) = push!(vec, elem)
+push_app!(vec, elem::Vector) = append!(vec, elem)
+"""
+Tracks a type for which `is_tracked_type` returns true, even across functions.
+"""
+function track_types!(m, expr::Expr, pointer_map)
+    head, args = expr.head, expr.args
+    if head === :new
+        # Constructors, tracking it's argument pointers
+        f_args = @view args[2:end]
+        ptrs = []
+        typ = args[1]
+        for i = 1:length(f_args)
+            arg = f_args[i]
+            fname = fieldname(typ, i)
+            track_types!(m, arg, pointer_map)
+            T = expr_type(m, arg)
+            if needs_tracking(m, T) && haskey(pointer_map, arg)
+                push_app!(ptrs, map(pointer_map[arg]) do ptr
+                    # append the field name
+                    (fname, ptr...)
+                end)
+            end
+        end
+        pointer_map[expr] = ptrs
+    elseif head in (:call, :curly) # curly and call are both call like
+        f_args = @view args[2:end]
+        args_need_tracking = false
+        ptrs = []
+        trackers = Dict()
+        for i = 1:length(f_args)
+            arg = f_args[i]
+            T = expr_type(m, arg)
+            if needs_tracking(m, T)
+                track_types!(m, arg, pointer_map)
+                haskey(pointer_map, arg) && push_app!(ptrs, pointer_map[arg])
+                args_need_tracking = true
+                trackers[TypedSlot(i + 1, T)] = get(pointer_map, arg, :failed_tracking)
+            end
+        end
+        @assert isa(args[1], LazyMethod)
+        f = resolve_func(m, args[1])
+        m2 = args[1]
+        m2.cache[:tracked_types] = trackers
+        if args_need_tracking # if args get pointers passed, we need to track the method
+            track_types!(m2, getast!(m2), trackers)
+        end
+        if isa(f, DataType) && f <: Tuple
+            # Tuples need special treatment since the constructor is intrinsic without using new
+            pointer_map[expr] = map(enumerate(ptrs)) do i_arg
+                i, arg = i_arg
+                (Symbol("field$i"), arg...)
+            end
+        elseif f == getfield
+            # getfield is special, since that's how we get pointers out of structs
+            field = args[3]
+            @assert length(ptrs) >= 1
+            newfields = []
+            for fields in ptrs
+                if isa(fields, Tuple) && !isempty(fields) && first(fields) == field
+                    # pop the head field, since getfield gets it out of the struct
+                    push!(newfields, Base.tail(fields))
+                end
+            end
+            pointer_map[expr] = newfields
+        end
+        if haskey(trackers, m2) # returns type with ptr
+            pointer_map[expr] = trackers[m2]
+        end
+    elseif head === :(=)
+        lhs, rhs = args
+        if needs_tracking(m, expr_type(m, lhs))
+            track_types!(m, rhs, pointer_map)
+            pointer_map[lhs] = get(pointer_map, rhs, :rhs_fail)
+        end
+    elseif head === :return && length(args) == 1
+        arg = args[1]
+        track_types!(m, arg, pointer_map)
+        if needs_tracking(m, expr_type(m, arg))
+            ret = get(pointer_map, arg, :return_fail)
+            pointer_map[m] = ret
+        end
+    else
+        track_types!(m, args, pointer_map)
+    end
+    pointer_map
+end
+
+
+
+function track_types!(m::LazyMethod)
+    args = to_tuple(m.signature[2])
+    pointer_map = Dict()
+    for i = 1:length(args)
+        T = args[i]
+        needs_tracking, fields = contains_tracked_type(m, T)
+        if needs_tracking
+            slot = TypedSlot(i + 1, T)
+            name = Sugar.slotname(m, slot)
+            unique_ptrs = map(enumerate(fields)) do i_field
+                (i_field[2]..., gensym(Symbol(string(name, i_field[1]))))
+            end
+            @show unique_ptrs
+            pointer_map[slot] = unique_ptrs
+        end
+    end
+    track_types!(m, getast!(m), pointer_map)
+    m.cache[:tracked_types] = pointer_map
+    return
+end
+
+
+#
+# using Transpiler: CLMethod
+# is_tracked_type(m::CLMethod, ::Type{<: Ptr}) = true
+#
+# struct DevBuff
+#     ptr::Ptr{Int}
+# end
+# struct DevView
+#     dev::DevBuff
+# end
+#
+# function invert(ptr1, ptr2)
+#     return (ptr2, ptr1)
+# end
+#
+# function test(x, y)
+#     ptr1 = x.dev.ptr
+#     ptr2 = y.dev.ptr
+#     inverted = invert(ptr1, ptr2)
+#     dev = y.dev
+#     dev2 = dev
+#     dev3 = DevBuff(dev2.ptr)
+#     lolz = identity(dev2)
+#     return inverted[1]
+# end
+#
+# m = CLMethod((test, Tuple{DevView, DevView}))
+# track_types!(m)
+#
+# m.cache[:tracked_types][TypedSlot(9, DevBuff)]
+# println(Sugar.getsource!(m))
